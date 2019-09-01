670ce45b0fae3664830c15cd0bdedd64219ddfa5
Stuart Taylor <stuartraetaylor@users.noreply.github.com>
2019-09-01 23:09:36 +0100

    Update for v5 password list

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ using the [downloadable pwned passwords list](https://haveibeenpwned.com/Passwor
 
 ## Usage
 
-1. Download [pwned-passwords-sha1-ordered-by-count-v4.7z.torrent](https://downloads.pwnedpasswords.com/passwords/pwned-passwords-sha1-ordered-by-count-v4.7z.torrent) (torrent), or use an [alternative download link](https://haveibeenpwned.com/Passwords).
+1. Download [pwned-passwords-sha1-ordered-by-count-v5.7z.torrent](https://downloads.pwnedpasswords.com/passwords/pwned-passwords-sha1-ordered-by-count-v5.7z.torrent) (torrent), or use an [alternative download link](https://haveibeenpwned.com/Passwords).
 
-2. Extract pwned-passwords-sha1-ordered-by-hash-v4.7z
+2. Extract pwned-passwords-sha1-ordered-by-hash-v5.7z
 
-        7z e pwned-passwords-sha1-ordered-by-hash-v4.7z
+        7z e pwned-passwords-sha1-ordered-by-hash-v5.7z
 
 3. Run pwned.sh
 
--- pwned.sh
+++ pwned.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MIT License - Copyright (c) 2019 Stuart Taylor - https://github.com/stuartraetaylor
 
-defaultPwnedfile="pwned-passwords-sha1-ordered-by-hash-v4.txt"
+defaultPwnedfile="pwned-passwords-sha1-ordered-by-hash-v5.txt"
 defaultLookbin="bin/look"
 
 dir=$PWD
@@ -11,7 +11,7 @@ echo "Using pwned file: ${pwnedfile/$dir\//}"
 
 if [ ! -f "$pwnedfile" ]; then
     echo "File not found"
-    return 2
+    exit 2
 fi
 
 lookbin="${2:-$dir/$defaultLookbin}"
@@ -19,10 +19,11 @@ echo "Using look command: ${lookbin/$dir\//}"
 
 if [ ! -f "$lookbin" ]; then
     echo "Command not found"
-    return 2
+    exit 2
 fi
 
-echo -n "Password: "
+echo
+echo -n "Pwned password: "
 read -s passwd
 echo "$(echo "$passwd" | sed -r 's/./*/g')"
 
@@ -30,19 +31,15 @@ result=$($lookbin -bf $(echo -n "$passwd" | sha1sum | cut -d' ' -f1) "$pwnedfile
 e=$?
 
 if [ $e -gt 1 ]; then
-    return $e
+    exit $e
 fi
 
 echo -n "Pwned: "
 
 if [ $e -eq 1 ]; then
     echo "not pwned."
-    return 1
+    exit 1
 fi
 
-#echo "$(echo -n "$result" | cut -d':' -f2) times"
-echo "pwned!"
-echo "$result" | cut -d':' -f2
-
-return 0
-
+echo "$(echo -n "$result" | sed -E 's/^[^:]+:([0-9]+).*$/\1/') times!"
+exit 0
